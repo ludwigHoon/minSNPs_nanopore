dc5897f6521fe65468cb5755876da4f203cc1b2a
Ludwig Kian Soon Hoon <30918181+ludwigHoon@users.noreply.github.com>
2023-08-19 13:31:07 +0000

    Scanned results files

---
+++ Scripts/aggregate_simulation_result.R
@@ -0,0 +1,66 @@
+### From most similar isolate in alignment to most likely CC based on SNP's differences
+###
+### FILLED_META_FILE: the metadata file in Results
+###
+###
+
+FILLED_META_FILE <- "all_meta_filled.csv"
+
+library(data.table)
+library(BiocParallel)
+meta <- fread(FILLED_META_FILE)[is.na(pubmlst_id)][, c("Isolate.name", "MLST.CC")]
+
+fin_res <- bplapply(c("random_400", "non_random_200"), function(dir, meta){
+    library(BiocParallel)
+    library(data.table)
+    
+    result <- list()
+
+    all_simulated_result <- list.files(path = dir, pattern = "^sim_S.*.csv", full.names = TRUE)
+    result <- bplapply(all_simulated_result, function(file){
+        details <- strsplit(basename(file), split = "_")
+        id <- gsub("S", "", sapply(details, `[`, 2))
+        if (dir == "random_400"){
+            snps_used <- sapply(details, `[`, 4)
+        } else{
+            n_sets <- c(1, 2, 3, 4, 5, 30, 79, 147, 200)
+            n_snps_set <- c(7, 13, 18, 23, 27, 99, 199, 300, 385)
+            snps_used <-  sapply(details, function(x) {n_snps_set[which(n_sets == as.numeric(x[4]))]})
+        }
+        sim_res_ind <- fread(file)
+        temp_result <- merge(sim_res_ind, meta, by.x = "result", by.y = "Isolate.name")
+        temp_result <- temp_result[temp_result[, .I[which.max(proportion_matched)], by=list(MLST.CC)]$V1]
+        temp_result$fin_score <- temp_result$proportion_matched * temp_result$proportion_scheme_found
+        temp_result <- temp_result[order(fin_score, decreasing = TRUE), list(MLST.CC, fin_score, proportion_matched, proportion_scheme_found, estimated_coverage, reads_used, reads_count)]
+        temp_result$snps_used <- snps_used
+        temp_result$snps_type <- dir
+        temp_result$pubmlst_id <- id
+        return(temp_result)
+    }, BPPARAM = MulticoreParam(workers = 32))
+    return(rbindlist(result))
+}, meta = meta, BPPARAM = BatchtoolsParam(workers = 391, cluster="slurm", template="~/slurm_bc_template.tmpl",
+        resources=list(walltime=60*60*24*5, ncpus=8)))
+all_result <- rbindlist(fin_res)
+
+colnames(all_result)[which(colnames(all_result) == "MLST.CC")] <- "predicted_MLST.CC"
+fwrite(all_result, "aggregated_simulation_result.csv", row.names = FALSE)
+to_collapse <- all_result[all_result[, .I[fin_score == max(fin_score)], by = list(pubmlst_id, snps_type, snps_used, reads_used)]$V1]
+collapsed_top_result <- to_collapse[,.(predicted_MLST.CC = paste0(predicted_MLST.CC, collapse = ","), reads_w_result = paste0(reads_count, collapse = ",")), by = list(pubmlst_id, snps_type, snps_used, reads_used, fin_score, proportion_matched, proportion_scheme_found, estimated_coverage)]
+newdata_meta <- fread(FILLED_META_FILE)[!is.na(pubmlst_id)][, c("pubmlst_id", "MLST.CC", "CC_filled")]
+newdata_meta$pubmlst_id <- as.character(newdata_meta$pubmlst_id)
+collapsed_top_result$pubmlst_id <- as.character(collapsed_top_result$pubmlst_id)
+RES <- merge(collapsed_top_result, newdata_meta, by = "pubmlst_id")
+RES$predicted_mlst_is_single <- sapply(RES$predicted_MLST.CC, function(x) {!grepl(",", x)})
+RES$predicted_mlst_is_correct <- sapply(seq_len(nrow(RES)), function(i){
+    if (RES$predicted_mlst_is_single[i] == TRUE){
+        return(RES$predicted_MLST.CC[i] == RES$CC_filled[i])
+    } else{
+        return(any(unlist(strsplit(RES$predicted_MLST.CC[i], ",")) == RES$CC_filled[i]))
+    }
+})
+fwrite(RES, "simulation_individual_results.csv", row.names = FALSE)
+summary <- RES[,.(n_correct = length(which(predicted_mlst_is_correct)),
+                n_single = length(which(predicted_mlst_is_single)),
+                n_single_correct = length(which(predicted_mlst_is_single & predicted_mlst_is_correct))),
+            by = list(reads_used, snps_used, snps_type)]
+fwrite(summary, "result_summary.csv", row.names = FALSE)
---
+++ Scripts/scan_simulated_read.R
@@ -0,0 +1,76 @@
+# PREV_RESULT_FILE can be downloaded from:
+# SSTRING_MINSNPS_200 
+# SSTRING_RANDOM_400
+# SLURM_TEMPLATE is a slurm bash template, for details, look at: https://rdrr.io/bioc/BiocParallel/f/inst/doc/Introduction_To_BiocParallel.pdf
+
+###### These files need to be updated to reflect the actual directory ####
+PREV_RESULT_FILE <- "sup_5_6_mega_hd_2_steps.txt"
+SSTRING_MINSNPS_200 <- "search_sequence_200_non_unique_SNPs.csv"
+SSTRING_RANDOM_400 <- "search_sequence_400_random_SNPs.csv"
+SLURM_TEMPLATE <- "slurm_bc_template.tmpl"
+######
+
+library(BiocParallel)
+bp <- BatchtoolsParam(workers = 391, cluster="slurm", template=SLURM_TEMPLATE,
+    resources=list(walltime=60*60*24*5, ncpus=4))
+
+sim_files <- list.files(pattern = ".*\\.fastq$")
+res <- bplapply(sim_files, function(file){
+    search_table2 <- read.csv(SSTRING_RANDOM_400)
+    search_table2 <- search_table2[search_table2$strand == "+", ]
+    search_table2$SNP <- sapply(strsplit(search_table2$id, split = "_"), `[`, 1)
+    library(minSNPs) 
+    library(BiocParallel)
+
+    # Look for 100, 200, 300, 400 SNPs
+    SNPs <- unique(search_table2$SNP)
+    for (max_snps in c(100, 200, 300, 400)){
+        search_table <- search_table2[search_table2$SNP %in% SNPs[1:max_snps], ]
+        previous_result <- NULL
+        for (read_i in c(0, 1, 2, 3, 4, 5, 6, 7)){
+
+            temp_result <- search_from_fastq_reads(fastq_file = file, search_tables = search_table, 
+                skip_n_reads = (0), progress = TRUE, max_n_reads = 1000,
+                quality_offset = 33, output_temp_result = FALSE, temp_result_folder = "./temp_results",
+                simplify_id = TRUE, output_read_length = TRUE, bp = BiocParallel::MulticoreParam(workers = 8)
+                )
+            combined_result <- combine_fastq_search_result(temp_result, search_table, previous_result, bp = MulticoreParam(workers = 2))
+            previous_result <- combined_result
+            inferred_result <- infer_from_combined(combined_result, search_table, 2800000)
+            tt <- inferred_result$result
+            tt$estimated_coverage <- rep(inferred_result$estimated_coverage, nrow(tt))
+            tt$reads_used <- rep( (1000*(read_i+1)), nrow(tt))
+            write.csv(tt, paste0("random_400/sim_", file, "_", max_snps, "_", read_i, ".csv"), row.names = FALSE)
+            }
+    }
+}, BPPARAM=bp)
+
+res <- bplapply(sim_files, function(file){
+    search_table2 <- read.csv(SSTRING_MINSNPS_200)
+    search_table2 <- search_table2[search_table2$strand == "+", ]
+    search_table2$SNP <- sapply(strsplit(search_table2$id, split = "_"), `[`, 1)
+    library(minSNPs)
+    library(BiocParallel)
+
+    snps_set <- process_result_file(PREV_RESULT_FILE)
+    # n_snps =  7  13  18  23  27  99 199 300 385
+    for (n_sets in c(1, 2, 3, 4, 5, 30, 79, 147, 200)){
+        search_table <- search_table2[search_table2$SNP %in% unique(unlist(snps_set[c(1:n_sets)])), ]
+        previous_result <- NULL
+        for (read_i in c(0, 1, 2, 3, 4, 5, 6, 7)){
+            temp_result <- search_from_fastq_reads(fastq_file = file, search_tables = search_table, 
+                skip_n_reads = (1000 * read_i), progress = TRUE, max_n_reads = 1000,
+                quality_offset = 33, output_temp_result = FALSE, temp_result_folder = "./temp_results",
+                simplify_id = TRUE, output_read_length = TRUE, bp = BiocParallel::MulticoreParam(workers = 8)
+                )
+            combined_result <- combine_fastq_search_result(temp_result, search_table, previous_result, bp = MulticoreParam(workers = 2))
+            previous_result <- combined_result
+            inferred_result <- infer_from_combined(combined_result, search_table, 2800000)
+            tt <- inferred_result$result
+            tt$estimated_coverage <- rep(inferred_result$estimated_coverage, nrow(tt))
+            tt$reads_used <- rep( (1000*(read_i+1)), nrow(tt))
+
+            write.csv(tt, paste0("non_random_200/sim_", file, "_", n_sets, "_", read_i, ".csv"), row.names = FALSE)
+            }
+    }
+}, BPPARAM=bp)
