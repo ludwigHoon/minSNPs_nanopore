7f88a341662cfd6601fc57f8ce2447409995972b
Ludwig Hoon <ldwgkshoon@gmail.com>
2023-09-19 17:03:56 +0930

    2023-09-19 update

--- Scripts/sketchy_comparison.R
+++ Scripts/sketchy_comparison.R
@@ -60,71 +60,17 @@ fwrite(agg_high_result, "sketchy_agg_result_10k.csv", row.names = FALSE)
 
 ### COMBINED ANALYSIS
 combined_result <- rbindlist(list(agg_default_result, agg_high_result))
-combined_result$mlst <- gsub("ST", "", combined_result$mlst)
+combined_result$mlst <- as.numeric(gsub("ST", "", combined_result$mlst))
 
 mlst_profiles <- fread("mlst_profiles.txt")
 mlst_profiles <- mlst_profiles[, c("ST", "clonal_complex")]
 
 fin_result <- merge(combined_result, mlst_profiles, by.x = "mlst", by.y = "ST")
-fin_result[clonal_complex == ""] <- paste0("CC", fin_result[clonal_complex == ""]$mlst)
+fin_result[clonal_complex == ""]$clonal_complex <- paste0("CC", fin_result[clonal_complex == ""]$mlst)
 
 
-needed_info <- fin_result[,c("pubmlst_id", "mlst", "clonal_complex", "shared_hashes", "meca", "pvl", "reads")]
-to_collapse <- needed_info[needed_info[, .I[shared_hashes == max(shared_hashes)], by = list(pubmlst_id, reads)]$V1]
-collapsed_top_result <- to_collapse[,.(predicted_MLST.CC = paste0(unique(clonal_complex), collapse = ","), predicted_MLST.ST = paste0(unique(mlst), collapse = ","), predicted_mecA = paste0(unique(meca), collapse = ","), predicted_pvl = paste0(unique(pvl), collapse = ","), shared_hashes = paste0(unique(shared_hashes), collapse = ",")), by = list(pubmlst_id, reads)]
-
-all_filled_meta <- fread(FILLED_META_FILE)[!is.na(pubmlst_id)][, c("pubmlst_id", "MLST.CC", "CC_filled")]
-
-all_filled_meta$pubmlst_id <- as.character(all_filled_meta$pubmlst_id)
-collapsed_top_result$pubmlst_id <- as.character(collapsed_top_result$pubmlst_id)
-
-merged_result <- merge(collapsed_top_result, all_filled_meta, by = "pubmlst_id")
-merged_result$predicted_mlst_is_single <- sapply(merged_result$predicted_MLST.CC, function(x) {!grepl(",", x)})
-merged_result$predicted_mlst_is_correct <- sapply(seq_len(nrow(merged_result)), function(i){
-    if (merged_result$predicted_mlst_is_single[i] == TRUE){
-        return(merged_result$predicted_MLST.CC[i] == merged_result$CC_filled[i])
-    } else{
-        return(any(unlist(strsplit(merged_result$predicted_MLST.CC[i], ",")) == merged_result$CC_filled[i]))
-    }
-})
-
-summary <- merged_result[,.(n_correct = length(which(predicted_mlst_is_correct)),
-                n_single = length(which(predicted_mlst_is_single)),
-                n_single_correct = length(which(predicted_mlst_is_single & predicted_mlst_is_correct))),
-            by = list(reads)]
-fwrite(summary, "sketchy_summary_1k.csv", row.names = FALSE)
-
-
-
-
-#### To run and complete
-all_result <- bplapply(all_simulated, function(file, SKETCHY_PATH){
-    library(data.table)
-    result_for_file <- list()
-    for (read_n in c(1,2,3,4,5,6,7,8)){
-        n_line <- read_n*4*1000
-        command <- paste0("sed -n 1,", n_line, "p ", file, " |")
-        command <- paste0(command, SKETCHY_PATH, " predict -r ~/sketchy/saureus_large/saureus_s10000_k16_full.msh -g ~/sketchy/saureus_large/genotypes_full.tsv -t 5 -H")
-        result_for_file[[read_n]] <- fread(text = system(command, intern = TRUE))
-    }
-    result_for_file <- rbindlist(result_for_file)
-    result_for_file$file <- file
-    return(result_for_file)
-}, SKETCHY_PATH= SKETCHY_PATH, BPPARAM = BatchtoolsParam(workers = 10, progress = TRUE, cluster="slurm", template=SLURM_TEMPLATE,
-        resources=list(walltime=60*60*24*5, ncpus=1)))
-
-agg_result <- rbindlist(all_result)
-agg_result$pubmlst_id <- gsub("S", "", gsub("_0001.fastq", "", agg_result$file))
-agg_result$file <- NULL
-fwrite(agg_result, "sketchy_agg_result_10k.csv", row.names = FALSE)
-mlst_profiles <- fread("mlst_profiles.txt")
-
-mlst_profiles$ST <- paste0("ST", mlst_profiles$ST)
-mlst_profiles <- mlst_profiles[, c("ST", "clonal_complex")]
-fin_result <- merge(agg_result, mlst_profiles, by.x = "mlst", by.y = "ST")
-needed_info <- fin_result[,c("pubmlst_id", "mlst", "clonal_complex", "shared_hashes", "meca", "pvl", "reads")]
-to_collapse <- needed_info[needed_info[, .I[shared_hashes == max(shared_hashes)], by = list(pubmlst_id, reads)]$V1]
-collapsed_top_result <- to_collapse[,.(predicted_MLST.CC = paste0(unique(clonal_complex), collapse = ","), predicted_MLST.ST = paste0(unique(mlst), collapse = ","), predicted_mecA = paste0(unique(meca), collapse = ","), predicted_pvl = paste0(unique(pvl), collapse = ","), shared_hashes = paste0(unique(shared_hashes), collapse = ",")), by = list(pubmlst_id, reads)]
+to_collapse <- fin_result[fin_result[, .I[shared_hashes == max(shared_hashes)], by = list(pubmlst_id, resolution, reads)]$V1]
+collapsed_top_result <- to_collapse[,.(predicted_MLST.CC = paste0(unique(clonal_complex), collapse = ","), predicted_MLST.ST = paste0(unique(mlst), collapse = ","), predicted_mecA = paste0(unique(meca), collapse = ","), predicted_pvl = paste0(unique(pvl), collapse = ","), shared_hashes = paste0(unique(shared_hashes), collapse = ",")), by = list(pubmlst_id, resolution, reads)]
 
 all_filled_meta <- fread(FILLED_META_FILE)[!is.na(pubmlst_id)][, c("pubmlst_id", "MLST.CC", "CC_filled")]
 
@@ -133,16 +79,24 @@ collapsed_top_result$pubmlst_id <- as.character(collapsed_top_result$pubmlst_id)
 
 merged_result <- merge(collapsed_top_result, all_filled_meta, by = "pubmlst_id")
 merged_result$predicted_mlst_is_single <- sapply(merged_result$predicted_MLST.CC, function(x) {!grepl(",", x)})
-merged_result$predicted_mlst_is_correct <- sapply(seq_len(nrow(merged_result)), function(i){
+merged_result$predicted_cc_is_correct <- sapply(seq_len(nrow(merged_result)), function(i){
     if (merged_result$predicted_mlst_is_single[i] == TRUE){
-        return(merged_result$predicted_MLST.CC[i] == merged_result$CC_filled[i])
+        return(merged_result$predicted_MLST.CC[i] %in% strsplit(merged_result$CC_filled[i], split = "/")[[1]])
     } else{
-        return(any(unlist(strsplit(merged_result$predicted_MLST.CC[i], ",")) == merged_result$CC_filled[i]))
+        return(any(unlist(strsplit(merged_result$predicted_MLST.CC[i], ",")) %in% strsplit(merged_result$CC_filled[i], split = "/")[[1]] ))
     }
 })
+merged_result$predicted_st_is_correct <- sapply(seq_len(nrow(merged_result)), function(i){
+    return(any(paste0("CC", unlist(strsplit(merged_result$predicted_MLST.ST[i], ","))) %in% strsplit(merged_result$CC_filled[i], split = "/")[[1]] ))
+})
+merged_result$predicted_mlst_is_correct <- merged_result$predicted_st_is_correct | merged_result$predicted_cc_is_correct
 
+### MAJOR LINEAGE ASSIGNMENT COMPARISON
+fwrite(merged_result, "sketchy_merged_result.csv", row.names = FALSE)
 summary <- merged_result[,.(n_correct = length(which(predicted_mlst_is_correct)),
                 n_single = length(which(predicted_mlst_is_single)),
                 n_single_correct = length(which(predicted_mlst_is_single & predicted_mlst_is_correct))),
-            by = list(reads)]
-fwrite(summary, "sketchy_summary_10k.csv", row.names = FALSE)
+            by = list(resolution, reads)]
+fwrite(summary, "sketchy_summary.csv", row.names = FALSE)
+
+### GENE DETECTION COMPARISON
